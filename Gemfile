ac854d4bec88b6b5ec38b56ee2c5fedbec72b25e
Marat Byte <maratbyte@ya.ru>
2022-02-10 17:30:57 +0300

    Configured database seeding

--- Gemfile
+++ Gemfile
@@ -34,10 +34,10 @@ gem 'cancancan'
 gem 'draper'
 gem 'pundit'
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'faker', '~> 2.19'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require_relative '../lib/fake_data.rb'
+
+10.times do
+  origin = FakeData::random_city
+  destination = FakeData::random_city
+  while origin == destination do
+    destination = FakeData::random_city
+  end
+  begin 
+    Parcel.create(
+      origin: origin,
+      destination: destination,
+      weight: Faker::Number.within(range: 1..100),
+      height: Faker::Number.within(range: 1..100),
+      length: Faker::Number.within(range: 1..100),
+      width: Faker::Number.within(range: 1..100),
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      phone_number: Faker::PhoneNumber.phone_number,
+      email: Faker::Internet.email
+    )
+  rescue => e
+    puts e
+  end
+end
